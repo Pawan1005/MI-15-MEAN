b17f0242a74d099e7a8929368d072868ff297f9b
Mrunal Itankar <itankarmrunal@gmail.com>
2022-07-07 11:11:25 +0530

    Javascript Assignment 1 Submitted.

---
+++ Mrunal_Itankar/JavaScript/Assignment_1/index_2.js
@@ -0,0 +1,6 @@
+function convert() {
+  var x = document.getElementById("temp").value;
+  y = parseFloat(x) * 1.8 + 32;
+  document.getElementById("celcius").textContent = x;
+  document.getElementById("fahrenheit").textContent = y;
+}
