4fa4263b06fcf697f963e280ef6fd5344aa8327e
Sanket-gupta0429 <106069695+Sanket-gupta0429@users.noreply.github.com>
2022-07-08 12:13:26 +0530

    Delete sanket_gupta/JAVASCRIPT/Assign1.js directory

--- sanket_gupta/JAVASCRIPT/Assign1.js/javas.js
+++
@@ -1,6 +0,0 @@
-function convert() {
-    var x = document.getElementById("temp").value;
-    y = parseFloat(x) * 1.8 + 32;
-    document.getElementById("celcius").textContent = x;
-    document.getElementById("fahrenheit").textContent = y;
-  }
