f277fa02614ad8d4974677fdcd3392ac932b6092
ajithgoud23 <105294259+ajithgoud23@users.noreply.github.com>
2022-07-14 11:50:01 +0530

    Add files via upload

---
+++ ajith goud"/JAVASCRIPT/Assin 2 questin  (1).js
@@ -0,0 +1,21 @@
+function largestNumber() {
+    var a = parseInt(document.getElementById("num1").value);
+    var b = parseInt(document.getElementById("num2").value);
+    var c = parseInt(document.getElementById("num3").value);
+    var d = parseInt(document.getElementById("num4").value);
+    var e = parseInt(document.getElementById("num5").value);
+    var first = 0;
+    var second = 0;
+    var arr = [a, b, c, d, e];
+    console.log(arr);
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] > first) {
+        second = first;
+        first = arr[i];
+      } else if (arr[i] > second && arr[i] < first) {
+        second = arr[i];
+      }
+    }
+    console.log(second);
+    document.getElementById("result").textContent = second;
+  }
