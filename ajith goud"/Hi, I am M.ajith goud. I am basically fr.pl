d9c620a313f633cba45a80cebb72c61083d08029
ajithgoud23 <105294259+ajithgoud23@users.noreply.github.com>
2022-07-14 15:01:02 +0530

    Add files via upload

---
+++ ajith goud"/Hi, I am M.ajith goud. I am basically fr.pl
@@ -0,0 +1,3 @@
+Hi, MY self M. ajith goud. I am basically from Nizamabad district and currently living in Hyderabad. I like to play cricket & esports games.
+I have completed my graduation in Computer Science branch from JNTUH University. 
+ I’m looking for a fresh start in IT sector.
