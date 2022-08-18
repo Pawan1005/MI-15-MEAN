7bdbb5babc81686d4eabe29e057158ef04f1f283
Mrunal Itankar <itankarmrunal@gmail.com>
2022-08-18 21:32:06 +0530

    Javascript Assignment 10 Submitted

--- Mrunal_Itankar/JavaScript/Assignment_10/index_3.js
+++ Mrunal_Itankar/JavaScript/Assignment_10/index_3.js
@@ -3,8 +3,8 @@ var value = validateEmail(str);
 function validateEmail(str){
     regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
  if (regexp.test(str)) {
-    document.write("Inserted address " + str + " is a valid email.")
+    document.write('Inserted address "' + str + '" is a valid email.')
   } else {
-    document.write("Inserted address " + str + " is not a valid email.")
+    document.write('Inserted address "' + str + '" is not a valid email.')
   }
 }
---
+++ Mrunal_Itankar/JavaScript/Assignment_10/index_8.js
@@ -0,0 +1,12 @@
+var str = prompt("Enter String.");
+var value = is_alphaDash(str);
+function is_alphaDash(str)
+{
+ regexp = /^[a-z0-9_\-]+$/i;
+  
+ if (regexp.test(str)) {
+    document.write('Inserted string "' + str + '" contains alpha, dash and underscore.');
+  } else {
+    document.write("Inserted string " + str + " does not contains alpha, dash and underscore.");
+  }
+}
