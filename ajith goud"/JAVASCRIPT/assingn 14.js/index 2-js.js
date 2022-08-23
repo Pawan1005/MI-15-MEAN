9e4581ed7a36b14d727f2ddc7303e9586ce666e8
Ajith Goud <105294259+ajithgoud23@users.noreply.github.com>
2022-08-23 13:58:28 +0530

    Add files via upload

---
+++ ajith goud"/JAVASCRIPT/assingn 14.js/index 2-js.js
@@ -0,0 +1,54 @@
+var markArr = [];
+var sumArr = [];
+function addMarks() {
+  var a = parseInt(document.getElementById("userinput1").value);
+  if (markArr.length < 10) {
+    if (a <= 80) {
+      markArr.push(a);
+    } else {
+      alert("Not allowed to enter marks more than 80.");
+    }
+  } else {
+    alert("Not allowed to enter subject marks more than 10.");
+  }
+}
+function showMarks() {
+  var list = "";
+  var sum = 0;
+  markArr.forEach(function (element, i) {
+    var calculation;
+    var index = i + 1;
+    calculation = parseInt(element + (20 / 100) * element);
+    sumArr.push(calculation);
+        list += "<li>Marks " + index + " : " + calculation + "</li>";
+    // console.log(calculation);
+    document.getElementById("subjectMarks").innerHTML = list;
+    document.getElementById("displayBeforeSorting").style.display = "block";
+  });
+}
+  function afterSortingMarks(){
+    var sortedArray = quickSort(sumArr);
+    // console.log(sortedArray);
+    var total = sortedArray.join(",");
+    document.getElementById("total").innerHTML = total;
+    document.getElementById("displayAfterSorting").style.display = "block";
+  }
+function quickSort(sumArr){
+if (sumArr.length <= 1) {
+  return sumArr;
+  } else {
+        var leftArr = [];              
+        var rightArr = [];
+        var newArr = [];
+        var pivot = sumArr.pop();      //  Take a pivot value
+        var length = sumArr.length;
+        for (var i = 0; i < length; i++) {
+           if (sumArr[i] <= pivot) {    // using pivot value start comparing
+              leftArr.push(sumArr[i]);      
+        } else {
+                rightArr.push(sumArr[i]);
+      }
+    }
+    return newArr.concat(quickSort(leftArr), pivot, quickSort(rightArr));   
+  }
+} 
