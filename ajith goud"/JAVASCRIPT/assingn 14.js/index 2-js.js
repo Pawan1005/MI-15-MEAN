e84480210d211baa8810316fe4fbc67f6e28e117
Ajith Goud <105294259+ajithgoud23@users.noreply.github.com>
2022-08-23 13:57:52 +0530

    Delete index 2-js.js

--- ajith goud"/JAVASCRIPT/assingn 14.js/index 2-js.js
+++
@@ -1,8 +0,0 @@
-let date = new Date(2021,0,31);
-getWeekDay(date);
-function getWeekDay(date){
-    let days = ['SU','MO','TU','WE','TH','FR','SA'];
-    // console.log(date.getDay());
-    var day = days[date.getDay()];
-    alert(day);
-}
