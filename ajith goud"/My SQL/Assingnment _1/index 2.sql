60f5e969c2df9470055b29774ce038ecb3923942
Ajith Goud <105294259+ajithgoud23@users.noreply.github.com>
2022-09-17 15:07:31 +0530

    Add files via upload

---
+++ ajith goud"/My SQL/Assingnment _1/index 2.sql
@@ -0,0 +1,14 @@
+create database studentTable;
+show databases;
+use studentTable;
+select database();
+create table course(courseId int primary key);
+CREATE TABLE student (
+    stdId INT PRIMARY KEY,
+    studName VARCHAR(20) NOT NULL,
+    studAadhar VARCHAR(12) UNIQUE,
+    mobileNo VARCHAR(8),
+    studCourseId int,
+    streams VARCHAR(10) CHECK (streams = 'science' OR 'commerce'),
+    FOREIGN KEY (studCourseId) REFERENCES course (courseId)
+);
---
+++ ajith goud"/My SQL/Assingnment _1/index 3.pl
@@ -0,0 +1,5 @@
+show databases;
+use studenttable;
+select database();
+alter table student add address varchar(30),add DOB Date;
+alter table student modify mobileNo VARCHAR(10);
